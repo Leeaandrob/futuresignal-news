764fc96d05652a1efea84bbd097887316f06e91e
Leandro Barbosa <leandrobar93@gmail.com>
2025-12-19 09:16:15 -0300

    feat: update domain

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://futuresignals.io',
+  site: 'https://futuresignals.news',
   integrations: [
     react(),
     tailwind({
